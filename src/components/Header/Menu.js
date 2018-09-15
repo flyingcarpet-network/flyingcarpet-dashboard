c3153c03b037d53aafd1b467a6e6f9a7164d33d9
bneiluj <julien.bouteloup@gmail.com>
2018-09-15 21:28:23 +0200

    Remove selection

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -6,36 +6,6 @@ import BountyModal from './../BountyModal';
 
 
 class Menu extends React.Component {
-    closest(el, selector) {
-        try {
-            let matchesFn;
-            // find vendor prefix
-            ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector'].some(function (fn) {
-                if (typeof document.body[fn] === 'function') {
-                    matchesFn = fn;
-                    return true;
-                }
-                return false;
-            });
-
-            let parent;
-
-            // traverse parents
-            while (el) {
-                parent = el.parentElement;
-                if (parent && parent[matchesFn](selector)) {
-                    return parent;
-                }
-                el = parent;
-            }
-        } catch (e) {
-          console.log('closest menu error');
-        }
-
-        return null;
-    }
-
-
     render() {
         return (
             <div className="app-main-menu d-none d-md-block">
