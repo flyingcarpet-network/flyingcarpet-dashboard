162f2d81969cb73aed42b1771a2b003200e4cf8c
Leopold Joy <leo@lotsa.ca>
2018-09-15 05:11:23 -0400

    fetch bounties via Web3 and display on heatmap

---
+++ src/actions/tcroActions-types.ts
@@ -0,0 +1,1 @@
+export const SET_BOUNTIES = 'tcroActions/SET_BOUNTIES';
--- src/utils/web3-utils.ts
+++ src/utils/web3-utils.ts
@@ -2,7 +2,9 @@
  * This file contains functions that aid in assessing the state of the web3 connection.
  */
 
+import Geohash from 'latlon-geohash';
 const EIP20JSON = require('./../contracts/abi/EIP20.json');
+const RegistryJSON = require('./../contracts/abi/Registry.json');
 const addresses = require('./../contracts/addresses.json');
 const contractAddresses = addresses.contracts;
 
@@ -53,5 +55,40 @@ export function getNitrogenBalance(web3, address) {
     const erc20Contract = new web3.eth.Contract(EIP20JSON.abi, erc20ContractAddress);
 
     return erc20Contract.methods.balanceOf(address).call().then(resolve).catch(reject);
+  });
+}
+
+/*
+ * @dev getBounties           Resolves an array of objects, each containing a the bounty data object,
+ *                            including a coordinates (lat/lon) field.
+ *                            (e.g.: [{title: "...", location: {lat: "...", lon: "..."}, ...}, ...] )
+ * @param web3                Web from Web3Provider injected by Ethereum-enabled browser.
+ */
+export function getBounties(web3) {
+  return new Promise((resolve, reject) => {
+    const registryContractAddress = contractAddresses.Registry;
+    const registryContract = new web3.eth.Contract(RegistryJSON.abi, registryContractAddress);
+
+    // Get the total number of bounties
+    return registryContract.methods.getNumBounties().call().then(numListings => {
+      const bountyData: any[] = []; // Bounty return array
+      if (numListings === 0) { return []; }
+
+      // Recursively iterate over listings (asynchronously)
+      return (function nextBounty (i) {
+        // Get bounty `data` field from listing
+        registryContract.methods.getBountyData(i - 1).call().then((dataString: string) => {
+          const dataObj: any = JSON.parse(dataString);
+
+          // Push bounty to return array
+          bountyData.push({
+            ...dataObj.payload,
+            coordinates: Geohash.decode(dataObj.payload.geohash)
+          });
+          if (--i) { return nextBounty(i); } // Decrement i and call nextBounty again if i > 0
+          else { return resolve(bountyData); } // Last iteration
+        });
+      })(numListings);
+    }).catch(reject);
   });
 }
